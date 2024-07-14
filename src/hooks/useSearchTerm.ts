f4375233fa01e6d12b2a4765bdaa955d9af7882d
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-07-14 21:54:19 +0500

    fix: fix useSearchTerm

--- src/hooks/useSearchTerm.ts
+++ src/hooks/useSearchTerm.ts
@@ -1,24 +1,19 @@
-import { Dispatch, SetStateAction, useEffect, useMemo, useState } from 'react';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 
 export const useSearchTerm = (
   key: string,
   initialValue: string = ''
 ): [string, Dispatch<SetStateAction<string>>] => {
-  const savedSearchTerm = localStorage.getItem(key);
   const [searchTerm, setSearchTerm] = useState(() => {
+    const savedSearchTerm = localStorage.getItem(key);
     return savedSearchTerm ? savedSearchTerm : initialValue;
   });
-
-  useEffect(() => {
-    if (savedSearchTerm) {
-      setSearchTerm(savedSearchTerm);
-    }
-  }, [key, savedSearchTerm]);
 
   useEffect(() => {
     const setLocalStorage = () => {
       localStorage.setItem(key, searchTerm);
     };
+
     setLocalStorage();
 
     return () => {
@@ -26,5 +21,5 @@ export const useSearchTerm = (
     };
   }, [key, searchTerm]);
 
-  return useMemo(() => [searchTerm, setSearchTerm], [searchTerm, setSearchTerm]);
+  return [searchTerm, setSearchTerm];
 };
