54b97ac25d1dee6a0cbe4d219b7328d46a990a56
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-07-17 20:06:07 +0500

    feat: add fetchSearchBooks

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import { URL_BOOK_SEARCH, URL_BOOK_DETAILS } from './constants';
 
 export const api = {
-  fetchData: async (pageNumber: number, pageSize: number) => {
+  fetchBooks: async (pageNumber: number, pageSize: number) => {
     const url: URL = new URL(URL_BOOK_SEARCH);
     const params: Record<string, number> = {
       pageNumber: pageNumber - 1,
@@ -10,6 +10,24 @@ export const api = {
     Object.keys(params).forEach((key) => url.searchParams.append(key, `${params[key]}`));
     return await fetch(url);
   },
+
+  fetchSearchBooks: async (pageSize: number, searchTerm: string = '') => {
+    const url: URL = new URL(URL_BOOK_SEARCH);
+    const options = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: `title=${encodeURIComponent(searchTerm)}`,
+    };
+    const params: Record<string, number> = {
+      pageSize,
+    };
+    Object.keys(params).forEach((key) => url.searchParams.append(key, `${params[key]}`));
+    return await fetch(url, options);
+  },
+
   fetchBookDetails: async (bookId: string) => {
     const url: URL = new URL(URL_BOOK_DETAILS);
     url.searchParams.append('uid', bookId);
