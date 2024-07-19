c9ba30166cbdfa45640e961454495e384ee6641c
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-07-19 09:42:48 +0500

    feat: add checkbox to the Card

--- src/components/SearchResult/Card/Card.tsx
+++ src/components/SearchResult/Card/Card.tsx
@@ -1,16 +1,36 @@
 import { FC } from 'react';
 import { CardProps } from './types';
 import './Card.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../../store';
+import { selectItem, unselectItem } from '../../../store/selectedItemsSlice';
 
 export const Card: FC<CardProps> = ({ book }) => {
+  const dispatch = useDispatch();
+  const selectedItems = useSelector((state: RootState) => state.selectedItems.selectedItems);
+  const isSelected = selectedItems.some((item) => item.uid === book.uid);
+
+  const handleCheckboxChange = () => {
+    if (isSelected) {
+      dispatch(unselectItem(book.uid));
+    } else {
+      dispatch(selectItem(book));
+    }
+  };
+
   const publishedYear = book.publishedYear || '';
   const numberOfPages = book.numberOfPages || '';
 
   return (
     <div className="card">
       <h2 className="card-title">{book.title}</h2>
-      <p>Year of publication: {publishedYear}</p>
-      <p>Number of pages: {numberOfPages}</p>
+      <div className="card-content">
+        <div className="card-description">
+          <p>Year of publication: {publishedYear}</p>
+          <p>Number of pages: {numberOfPages}</p>
+        </div>
+        <input type="checkbox" checked={isSelected} onChange={handleCheckboxChange} />
+      </div>
     </div>
   );
 };
