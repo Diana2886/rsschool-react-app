591dce7509803d3eee4fc5cea9e6bd090086bb22
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-07-19 22:06:44 +0500

    refactor: change interface to type for props

--- src/components/SearchResult/Card/types.ts
+++ src/components/SearchResult/Card/types.ts
@@ -1,5 +1,5 @@
 import { Book } from '../../../services/bookApi/types';
 
-export interface CardProps {
+export type CardProps = {
   book: Book;
-}
+};
--- src/components/SearchResult/types.ts
+++ src/components/SearchResult/types.ts
@@ -1,5 +1,5 @@
 import { Book } from '../../services/bookApi/types';
 
-export interface SearchResultProps {
+export type SearchResultProps = {
   books: Book[];
-}
+};
