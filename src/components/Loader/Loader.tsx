2156d64a2978bb5ccf408a41183769532a705df1
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-07-24 22:07:46 +0500

    refactor: change styles

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,5 +2,5 @@ import { FC } from 'react';
 import './Loader.scss';
 
 export const Loader: FC = () => {
-  return <h3 className="loader">Loading...</h3>;
+  return <h2 className="loader">Loading...</h2>;
 };
