0c5ab3e7cec55f0006fdc434e84848fb1440fd40
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-07-30 17:01:48 +0500

    feat: change useSearchTerm

--- src/hooks/useSearchTerm.ts
+++ src/hooks/useSearchTerm.ts
@@ -4,21 +4,18 @@ export const useSearchTerm = (
   key: string,
   initialValue: string = ''
 ): [string, Dispatch<SetStateAction<string>>] => {
-  const [searchTerm, setSearchTerm] = useState(() => {
-    const savedSearchTerm = localStorage.getItem(key);
-    return savedSearchTerm ? savedSearchTerm : initialValue;
+  const [searchTerm, setSearchTerm] = useState<string>(() => {
+    if (typeof window !== 'undefined') {
+      const savedSearchTerm = localStorage.getItem(key);
+      return savedSearchTerm ? savedSearchTerm : initialValue;
+    }
+    return initialValue;
   });
 
   useEffect(() => {
-    const setLocalStorage = () => {
+    if (typeof window !== 'undefined') {
       localStorage.setItem(key, searchTerm);
-    };
-
-    setLocalStorage();
-
-    return () => {
-      setLocalStorage();
-    };
+    }
   }, [key, searchTerm]);
 
   return [searchTerm, setSearchTerm];
