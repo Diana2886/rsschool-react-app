d3573f2abca6f99dc9689d29a2ee7079250fb522
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-08-01 14:44:13 +0500

    fix: fix icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="/static/book.ico" />
+        {/* <link rel="icon" type="image/png" sizes="16x16" href="/static/book.png" /> */}
+      </Head>
       <body>
         <Main />
         <NextScript />
