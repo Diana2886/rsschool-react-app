c215ed7a7ba187a24af8ac4b9a8d597cb288f831
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-08-01 08:42:26 +0500

    feat: add loader to the _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,15 +4,40 @@ import { store } from '../store';
 import { ThemeProvider } from '../context/ThemeContext';
 import '@/styles/globals.scss';
 import { ErrorBoundary } from '@/components/ErrorBoundary';
+import { NextPage } from 'next';
+import { ReactNode } from 'react';
+import { Loader } from '@/components/Loader';
+import { usePageLoading } from '@/hooks/usePageLoading';
 
-export default function App({ Component, pageProps }: AppProps) {
+type Page<P = object> = NextPage<P> & {
+  getLayout?: (page: ReactNode) => ReactNode;
+};
+
+type Props = AppProps & {
+  Component: Page;
+};
+
+const App = ({ Component, pageProps }: Props) => {
+  const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
+
+  const { isLoading } = usePageLoading();
+
   return (
-    <Provider store={store}>
-      <ErrorBoundary>
-        <ThemeProvider>
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </ErrorBoundary>
-    </Provider>
+    <>
+      <Provider store={store}>
+        <ErrorBoundary>
+          <ThemeProvider>
+            {getLayout(
+              <>
+                {isLoading && <Loader />}
+                <Component {...pageProps} />
+              </>
+            )}
+          </ThemeProvider>
+        </ErrorBoundary>
+      </Provider>
+    </>
   );
-}
+};
+
+export default App;
