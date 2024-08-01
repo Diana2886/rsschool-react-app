3ab57e8e74c7059c86e0ef9c6c564f4c1f484fe3
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-08-01 13:40:50 +0500

    refactor: change MainLayout

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -2,33 +2,15 @@ import { FC } from 'react';
 import { MainLayoutProps } from './types';
 import { Header } from '@/views/MainPage/Header';
 import { useTheme } from '@/hooks/useTheme';
-import { useSearchTerm } from '@/hooks/useSearchTerm';
-import { FIRST_PAGE_NUMBER, LOCAL_STORAGE_KEY } from '@/views/MainPage/Main/constants';
-import { useRouter } from 'next/router';
-import { Search } from '@/components/Search';
-import styles from './MainLayout.module.scss';
 
 export const MainLayout: FC<MainLayoutProps> = ({ children }) => {
   const { theme, toggleTheme } = useTheme();
-  const [, setSearchTerm] = useSearchTerm(LOCAL_STORAGE_KEY);
-
-  const router = useRouter();
-
-  const handleSearch = (searchTerm: string) => {
-    setSearchTerm(searchTerm);
-    router.push(`?page=${FIRST_PAGE_NUMBER}${searchTerm ? `&search=${searchTerm}` : ''}`);
-  };
 
   return (
     <>
       <div className={`app ${theme}`}>
         <Header toggleTheme={toggleTheme} />
-        <main>
-          <Search onSearchClick={handleSearch} />
-          <div className={styles['main-content']} data-testid={'main-content'}>
-            {children}
-          </div>
-        </main>
+        {children}
       </div>
     </>
   );
