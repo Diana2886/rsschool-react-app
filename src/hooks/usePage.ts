394669f1bc8ebd59f8466abc062e1dff476d1660
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-08-01 13:39:23 +0500

    refactor: change usePage

--- src/hooks/usePage.ts
+++ src/hooks/usePage.ts
@@ -1,15 +1,9 @@
 import { FIRST_PAGE_NUMBER } from '../views/MainPage/Main/constants';
-import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
 export const usePage = () => {
-  const [pageNumber, setPageNumber] = useState(FIRST_PAGE_NUMBER);
   const router = useRouter();
-
-  useEffect(() => {
-    const page = parseInt((router.query.page as string) || `${FIRST_PAGE_NUMBER}`, 10);
-    setPageNumber(page);
-  }, [router]);
+  const pageNumber = parseInt((router.query.page as string) || `${FIRST_PAGE_NUMBER}`, 10);
 
   return pageNumber;
 };
