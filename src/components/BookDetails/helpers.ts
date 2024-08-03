02f67d471e4ce2b4a6659a64b7329a3d5f2e8cf6
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-08-03 23:13:45 +0500

    refactor: change helpers in bookDetails

--- src/components/BookDetails/helpers.ts
+++ src/components/BookDetails/helpers.ts
@@ -1,11 +1,10 @@
 import { Entries } from './types';
 
+export const hasEntries = (entries: Entries) => entries && entries.length > 0;
+
 export const getEntriesString = (entries: Entries) => {
-  if (entries.length) {
+  if (hasEntries(entries)) {
     return entries.map((entry) => entry.name).join(', ');
   }
-
   return '';
 };
-
-export const hasEntries = (entries: Entries) => entries && entries.length > 0;
