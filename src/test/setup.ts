333eb4fd20dd1b4524d9178ed164baa99fa53bb3
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-08-03 17:28:59 +0500

    feat: change test setup

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,17 +1,26 @@
-import { afterAll, afterEach, beforeAll } from 'vitest';
-import { cleanup } from '@testing-library/react';
+import { /*  afterAll, afterEach, beforeAll, */ vi } from 'vitest';
+// import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
-import { setupStore } from '../store';
-import { server } from './__ mocks __/api/server';
-import { bookApi } from '../services/bookApi';
+// import { setupStore } from '../store';
+// // import { server } from './__ mocks __/api/server';
+// import { bookApi } from '../services/bookApi';
+import { book } from './__ mocks __/book';
 
-const store = setupStore({});
+vi.mock('@/services/getBooksData', () => ({
+  getBooksData: vi.fn(),
+}));
 
-beforeAll(() => server.listen());
-afterEach(() => {
-  server.resetHandlers();
-  store.dispatch(bookApi.util.resetApiState());
+vi.mock('@/services/getBookDetails', () => ({
+  getBookDetails: vi.fn().mockResolvedValue(book),
+}));
 
-  cleanup();
-});
-afterAll(() => server.close());
+// const store = setupStore({});
+
+// beforeAll(() => server.listen());
+// afterEach(() => {
+//   server.resetHandlers();
+//   store.dispatch(bookApi.util.resetApiState());
+
+//   cleanup();
+// });
+// afterAll(() => server.close());
