a2d2ed682167070ac9f087fe131fc6812da40ed1
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-08-05 16:28:09 +0500

    refactor: change useSearchTerm

--- src/hooks/useSearchTerm.ts
+++ src/hooks/useSearchTerm.ts
@@ -1,22 +1,21 @@
+import { LOCAL_STORAGE_KEY } from '@/components/Main/constants';
 import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 
-export const useSearchTerm = (
-  key: string,
-  initialValue: string = ''
-): [string, Dispatch<SetStateAction<string>>] => {
+export const useSearchTerm = (): [string, Dispatch<SetStateAction<string>>] => {
+  const key = LOCAL_STORAGE_KEY;
+
   const [searchTerm, setSearchTerm] = useState<string>(() => {
     if (typeof window !== 'undefined') {
       const savedSearchTerm = localStorage.getItem(key);
-      return savedSearchTerm ? savedSearchTerm : initialValue;
+      return savedSearchTerm ? savedSearchTerm : '';
     }
-    return initialValue;
+    return '';
   });
 
   useEffect(() => {
     const setLocalStorage = () => {
       localStorage.setItem(key, searchTerm);
     };
-
     setLocalStorage();
 
     return () => {
