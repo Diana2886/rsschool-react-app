536b18de15bfe866ad23df99e09688968e4f0090
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-08-06 14:19:10 +0500

    feat: add vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,28 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { resolve } from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
+    coverage: {
+      exclude: [
+        'next.config.mjs',
+        'node_modules/**',
+        'dist/**',
+        '.eslintrc.cjs',
+        '.prettierrc.cjs',
+        'vite.config.ts',
+      ],
+    },
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
   },
 });
