e1a8f0f647fa890059ef08304e1c97e240eabf3b
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-08-06 14:22:39 +0500

    refactor: remove unneeded files

--- src/layouts/BookDetailsLayout/BookDetailsLayout.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from 'react';
-import { BookDetailsLayoutProps } from './types';
-import styles from './BookDetailsLayout.module.scss';
-
-export const BookDetailsLayout: FC<BookDetailsLayoutProps> = ({ children }) => {
-  return <div className={styles['right-section']}>{children}</div>;
-};
