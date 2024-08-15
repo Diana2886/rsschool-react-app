2ba0452750e79c600a142ce5aa279bebb5536408
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-08-15 20:46:47 +0500

    feat: add routes and styles to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Main } from './components/Main';
+import { UncontrolledForm } from './components/UncontrolledForm';
+import { HookForm } from './components/HookForm';
+import { Header } from './components/Header';
 import './App.css';
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/uncontrolled-form" element={<UncontrolledForm />} />
+        <Route path="/hook-form" element={<HookForm />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
