10668222549ff7e6012dddae97a222ffef2b8376
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-08-17 11:38:32 +0500

    refactor: rename HookForm to ControlledForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Main } from './components/Main';
 import { UncontrolledForm } from './components/UncontrolledForm';
-import { HookForm } from './components/HookForm';
+import { ControlledForm } from './components/ControlledForm';
 import { Header } from './components/Header';
 import './App.css';
 
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/uncontrolled-form" element={<UncontrolledForm />} />
-        <Route path="/hook-form" element={<HookForm />} />
+        <Route path="/controlled-form" element={<ControlledForm />} />
       </Routes>
     </BrowserRouter>
   );
